9d2ff3afa7ec6269ddc07d43f3b9d68fe22b898b
Evan Araki <112609431+EvanAraki@users.noreply.github.com>
2022-10-18 11:20:58 -1000

    Completed Murphy's Mockup

---
+++ app/imports/ui/pages/Murphys.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Middle from '../components/Middle';
+
+const Murphys = () => (
+  <>
+    <div className="d-flex flex-column min-vh-100">
+      <Middle />
+    </div>
+  </>
+);
+
+export default Murphys;
+
+
